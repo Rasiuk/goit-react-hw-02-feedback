94a9ebca79141a56671566114374983974024581
Evgeniy <gerardcobain1@gmail.com>
2023-02-22 19:12:43 +0200

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Statistic } from './Statistic/Statistic';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  clickOnGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+  clickOnNeutral = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+  clickOnbad = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <button type="button" onClick={this.clickOnGood}>
+          Good
+        </button>
+        <button type="button" onClick={this.clickOnNeutral}>
+          Neutral
+        </button>
+        <button type="button" onClick={this.clickOnbad}>
+          bad
+        </button>
+        <Statistic
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+        />
+      </div>
+    );
+  }
+}
