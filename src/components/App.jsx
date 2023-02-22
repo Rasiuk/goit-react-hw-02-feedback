9ef432f204b3732480c284428633764d30abd63a
Evgeniy <gerardcobain1@gmail.com>
2023-02-22 23:45:28 +0200

    create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import { Statistic } from './Statistic/Statistic';
+import { OptionsBtn } from './FeedbackOptions/FeedbackOptions';
+import { Section } from 'Section/Section';
+import { Notification } from './Notification/Notificatin';
 export class App extends Component {
   state = {
     good: 0,
@@ -10,35 +13,59 @@ export class App extends Component {
     this.setState(prevState => {
       return { good: prevState.good + 1 };
     });
+    this.countTotalFeedback();
   };
   clickOnNeutral = () => {
     this.setState(prevState => {
       return { neutral: prevState.neutral + 1 };
     });
+    this.countTotalFeedback();
   };
   clickOnbad = () => {
     this.setState(prevState => {
       return { bad: prevState.bad + 1 };
     });
+    this.countTotalFeedback();
   };
+  countTotalFeedback = () => {
+    this.setState(prevState => {
+      return { total: prevState.good + prevState.neutral + prevState.bad };
+    });
+    this.countPositiveFeedbackPercentage();
+  };
+  countPositiveFeedbackPercentage() {
+    this.setState(prevState => {
+      let totalFeedback = Math.round((prevState.good * 100) / prevState.total);
+      return {
+        percentage: totalFeedback,
+      };
+    });
+  }
   render() {
+    const { good, neutral, bad, total, percentage } = this.state;
     return (
       <div>
-        <h1>Please leave feedback</h1>
-        <button type="button" onClick={this.clickOnGood}>
-          Good
-        </button>
-        <button type="button" onClick={this.clickOnNeutral}>
-          Neutral
-        </button>
-        <button type="button" onClick={this.clickOnbad}>
-          bad
-        </button>
-        <Statistic
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-        />
+        <Section title="Please leave feedback">
+          <OptionsBtn
+            clickOnGood={this.clickOnGood}
+            clickOnNeutral={this.clickOnNeutral}
+            clickOnbad={this.clickOnbad}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {!good && !neutral && !bad ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistic
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={percentage}
+            />
+          )}
+        </Section>
       </div>
     );
   }
--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -1,9 +1,25 @@
-export const Statistic = ({ good, neutral, bad }) => {
+import PropTypes from 'prop-types';
+export const Statistic = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
   return (
     <div>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>percentage: {positivePercentage}%</p>
     </div>
   );
+};
+Statistic.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
